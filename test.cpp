8e49bd55ecb326ca54b2e3bbe2b3796780c84959
doujiangjiediqi <47945811+doujiangjiediqi@users.noreply.github.com>
2022-02-16 19:32:10 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <time.h>
+#include "../sylar/log.h"
+int main(int argc, char **argv)
+{
+    sylar::Logger::ptr logger(new sylar::Logger);
+    logger->addAppender(sylar::LogAppender::ptr(new sylar::StdoutLogAppender));
+    sylar::LogEvent::ptr event(new sylar::LogEvent(__FILE__, __LINE__, 0, 1, 2, time(0)));
+    logger->log(sylar::LogLevel::DEBUG, event);
+    std::cout << "hello log" << std::endl;
+    return 0;
+}
